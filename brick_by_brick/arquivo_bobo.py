eaf13285ce85db6c48231b4abbe13ddf86adcd28
azevedo <azevedoantoniocicero@gmail.com>
2024-02-28 15:15:30 -0300

    diretorio para começar o desenvolvimento da logica de programaçao do moduloe um arquivo de exemplo para a galera entender como funciona mais ou menos a criançao de uma função com boas práticas

---
+++ brick_by_brick/arquivo_bobo.py
@@ -0,0 +1,21 @@
+"""
+Arquivo para elucidar como funciona os testes
+"""
+
+
+def funcao_boba(a: int) -> int:
+    """
+    Função que retorna a variavel de entrada do parâmetro
+
+    Parameters:
+        a: Inteiro que será retornado
+
+    Returns:
+        O mesmo inteiro que passou pelo nosso parâmetro
+
+    Examples:
+        >>> a = 10
+        >>> funcao_boba(a)
+        10
+    """
+    return a
